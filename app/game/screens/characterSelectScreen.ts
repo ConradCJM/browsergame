15df262989baeba21587be5c355252e6213ffc07
CJ Mejia <cjconradjoshmejia@gmail.com>
2026-05-27 18:11:01 -0400

    initialize file for character screen select

---
+++ app/game/screens/characterSelectScreen.ts
@@ -0,0 +1,15 @@
+import {Screen} from '@/app/game/screens/screenInterface';
+import {Input} from '@/app/game/systems/input';
+
+export class CharacterSelectScreen implements Screen {
+
+    update(dt: number): void {
+    }
+
+    handleInput(input: Input): void {
+    }
+
+    render(ctx: CanvasRenderingContext2D): void {
+    }
+    
+}
